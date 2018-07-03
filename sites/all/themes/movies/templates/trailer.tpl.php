4faceba4bd4d9132b56b47c6c1ff93f42d2a4d0f
Senya <suharchuk.dima@gmail.com>
2018-07-03 19:23:52 +0300

    Added field "Trailer" to node film. Display link to trailers. Display trailers in popup. Refactored code

---
+++ sites/all/themes/movies/templates/trailer.tpl.php
@@ -0,0 +1,5 @@
+<?php global $language; ?>
+<iframe width="100%" height="400"
+        src="https://www.youtube-nocookie.com/embed/<?php print $hash; ?>?rel=0&autoplay=1&controls=2&color=white&showinfo=0&iv_load_policy=3&hl=<?php print $language->language; ?>"
+        frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+</iframe>
